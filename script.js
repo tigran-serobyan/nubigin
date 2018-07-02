58a1f793f78448f0e857e304533b661f5680c297
Your Name <you@example.com>
2018-07-02 21:56:54 +0400

    Ubdate from home computer, error with ip 02.07.2018

--- script.js
+++ script.js
@@ -1,9 +1,48 @@
 function main() {
     var socket = io.connect('http://localhost:3000');
+    var myIp;
+    function getUserIP(onNewIP) { //  onNewIp - your listener function for new IPs
+        //compatibility for firefox and chrome
+        var myPeerConnection = window.RTCPeerConnection || window.mozRTCPeerConnection || window.webkitRTCPeerConnection;
+        var pc = new myPeerConnection({
+            iceServers: []
+        }),
+            noop = function () { },
+            localIPs = {},
+            ipRegex = /([0-9]{1,3}(\.[0-9]{1,3}){3}|[a-f0-9]{1,4}(:[a-f0-9]{1,4}){7})/g,
+            key;
+
+        function iterateIP(ip) {
+            if (!localIPs[ip]) onNewIP(ip);
+            localIPs[ip] = true;
+        }
+
+        //create a bogus data channel
+        pc.createDataChannel("");
+
+        // create offer and set local description
+        pc.createOffer().then(function (sdp) {
+            sdp.sdp.split('\n').forEach(function (line) {
+                if (line.indexOf('candidate') < 0) return;
+                line.match(ipRegex).forEach(iterateIP);
+            });
+
+            pc.setLocalDescription(sdp, noop, noop);
+        }).catch(function (reason) {
+            // An error occurred, so handle the failure to connect
+        });
+
+        //listen for candidate events
+        pc.onicecandidate = function (ice) {
+            if (!ice || !ice.candidate || !ice.candidate.candidate || !ice.candidate.candidate.match(ipRegex)) return;
+            ice.candidate.candidate.match(ipRegex).forEach(iterateIP);
+        };
+    }
+
     var input = document.getElementById('post_text');
     var button = document.getElementById('post_submit');
-    var user = "areg";
-    var following = ['tigran','areg','vanuhi'];
+    var user = "tigran";
+    var following;
     var posts = document.getElementById('posts');
     function appPost() {
         var val = input.value;
@@ -19,23 +58,45 @@ function main() {
     }
     button.onclick = appPost;
     function newPost(data) {
-        for(var i in following){
-            if(following[i] == data.user){
-                             
+        for (var i in following) {
+            if (following[i] == data.user) {
+
                 var div = document.createElement('div');
-                div.innerHTML = '<h3 class="post-text">'+data.user+'</h3><p>'+data.text+'</p>';
-               posts.insertBefore(div, posts.childNodes[0]);
+                div.innerHTML = '<h3 class="post-text">' + data.user + '</h3><p>' + data.text + '</p>';
+                posts.insertBefore(div, posts.childNodes[0]);
             }
         }
-        
+
     }
-    function deletE(count){
-        if(count == "all"){
-        posts.innerHTML = "";
+    function deletE(count) {
+        if (count == "all") {
+            posts.innerHTML = "";
+        }
+    }
+    function userControl(data) {
+        for (var i in data[0]) {
+            if (data[0][i] == user) {
+                following = data[1][i];
+            }
+
+        }
+    }
+    function signIn(data) {
+        for (var i in data) {
+            if (data[i].ip == myIp) {
+                user = data.user;
+                socket.emit('start','start');
+                // getUserIP(function (ip) {
+                //     myIp = ip;
+                //     socket.emit("ip", [ip, user]);
+                // });
+            }
         }
     }
     socket.on('new post', newPost);
     socket.on('delete', deletE);
+    socket.on('users', userControl);
+    socket.on('signIn', signIn);
 } // main closing bracket
 
 window.onload = main;
--- server.js
+++ server.js
@@ -22,15 +22,17 @@ io.on('connection', function (socket) {
 		else {
 		}
 	});
-	setTimeout(() => {
+	function start() {
 		io.sockets.emit("delete", "all");
 		json = JSON.parse(json);
+		io.sockets.emit("users", [json.users, json.following, json.followers]);
 		for (var i in json.p) {
 			io.sockets.emit("new post", json.p[i]);
 		}
-	}, 200);
+	}
+	setTimeout(function(){io.sockets.emit("signIn", json.singIn);},10);
 
-
+	socket.on("start", start)
 	socket.on("new post", function (data) {
 
 		fs.readFile(file, 'utf8', function (err, data) {
@@ -46,6 +48,22 @@ io.on('connection', function (socket) {
 			json = JSON.stringify(json);
 			fs.writeFile(file, json);
 			io.sockets.emit("new post", data);
-		}, 1000)
+		}, 10)
+	})
+	socket.on("ip", function (data) {
+
+		fs.readFile(file, 'utf8', function (err, data) {
+			if (!err) {
+				json = data;
+			}
+			else {
+			}
+		});
+		setTimeout(function () {
+			json = JSON.parse(json);
+			json.signIn.push({ ip: data[0], user: data[1] });
+			json = JSON.stringify(json);
+			fs.writeFile(file, json);
+		}, 10)
 	})
 });
